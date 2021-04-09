bea32085b4ea8230dbafafb074c5a0d3b7705b58
JanSharp <steinmeister36@gmail.com>
2021-04-09 02:10:38 +0200

    Change args to fit this project

--- args_service.lua
+++ args_service.lua
@@ -3,13 +3,8 @@ local args_util = require("args_util")
 local Path = require("path")
 
 ---@class Args
----@field project_name string
----@field project_id string @ same as project name but as a valid lua identifier
----@field source_extensions table<string, boolean>
 ---@field source_dir_path Path
----@field target_extension string
 ---@field target_dir_path Path
----@field auto_clean_up_target_dir boolean
 
 ---@param arg string[]
 ---@return Args
@@ -37,59 +32,49 @@ local function get_args(arg)
     )
   end
 
-  ---@param group_name string
-  local function convert_to_map(group_name)
-    local result = {}
-    for _, v in ipairs(args[group_name]) do
-      result[v] = true
-    end
-    args[group_name] = result
-  end
+  -- ---@param group_name string
+  -- local function convert_to_map(group_name)
+  --   local result = {}
+  --   for _, v in ipairs(args[group_name]) do
+  --     result[v] = true
+  --   end
+  --   args[group_name] = result
+  -- end
 
   ---@param group_name string
   local function single(group_name)
     assert(#args[group_name] == 1, "Program args group '--"
       ..get_original_group_name(group_name).."' must contain one single value."
     )
+    ---@type string
     args[group_name] = args[group_name][1]
   end
 
-  ---@param group_name string
-  local function flag(group_name)
-    if args[group_name] then
-      assert(#args[group_name] == 0, "Program args group '--"
-        ..get_original_group_name(group_name)
-        .."' must contain 0 subsequent values. It's a flag."
-      )
-      args[group_name] = true
-    else
-      args[group_name] = false
-    end
-  end
+  -- ---@param group_name string
+  -- local function flag(group_name)
+  --   if args[group_name] then
+  --     assert(#args[group_name] == 0, "Program args group '--"
+  --       ..get_original_group_name(group_name)
+  --       .."' must contain 0 subsequent values. It's a flag."
+  --     )
+  --     args[group_name] = true
+  --   else
+  --     args[group_name] = false
+  --   end
+  -- end
 
   rename_groups()
 
-  assert_group("project_name")
-  assert_group("source_extensions")
   assert_group("source_dir")
-  assert_group("target_extension")
   assert_group("target_dir")
 
-  convert_to_map("source_extensions")
-
-  single("project_name")
   single("source_dir")
-  single("target_extension")
   single("target_dir")
 
-  ---@type string
-  args.project_id = args.project_name:gsub("[^a-zA-Z0-9_]", "_")
   args.source_dir_path = Path.new(args.source_dir)
   args.source_dir = nil
   args.target_dir_path = Path.new(args.target_dir)
   args.target_dir = nil
-
-  flag("auto_clean_up_target_dir")
 
   return args
 end
--- main.lua
+++ main.lua
@@ -2,5 +2,8 @@
 ---@type LFS
 local lfs = require("lfs")
 local json = require("json")
+local args_service = require("args_service")
+
+local args = args_service.get_args(arg)
 
 print(json.decode(json.encode("hello world")))
