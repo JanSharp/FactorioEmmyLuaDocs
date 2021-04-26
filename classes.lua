0383eeebfe85b2c3402e15a70af8719d08980062
JanSharp <steinmeister36@gmail.com>
2021-04-26 23:36:44 +0200

    Fix LFS EmmyLua comment newlines

--- classes.lua
+++ classes.lua
@@ -35,34 +35,63 @@ local LFSDirObject = {
 ---@class LFS
 local LFS = {
   ---Returns a table with the file attributes corresponding to filepath (or nil followed by an error message and a system-dependent error code in case of error). If the second optional argument is given and is a string, then only the value of the named attribute is returned (this use is equivalent to lfs.attributes(filepath)[request_name], but the table is not created and only one attribute is retrieved from the O.S.). if a table is passed as the second argument, it (result_table) is filled with attributes and returned instead of a new table. The attributes are described as follows; attribute mode is a string, all the others are numbers, and the time related attributes use the same time reference of os.time:
+  ---
   ---dev
+  ---
   ---on Unix systems, this represents the device that the inode resides on. On Windows systems, represents the drive number of the disk containing the file
+  ---
   ---ino
+  ---
   ---on Unix systems, this represents the inode number. On Windows systems this has no meaning
+  ---
   ---mode
+  ---
   ---string representing the associated protection mode (the values could be file, directory, link, socket, named pipe, char device, block device or other)
+  ---
   ---nlink
+  ---
   ---number of hard links to the file
+  ---
   ---uid
+  ---
   ---user-id of owner (Unix only, always 0 on Windows)
+  ---
   ---gid
+  ---
   ---group-id of owner (Unix only, always 0 on Windows)
+  ---
   ---rdev
+  ---
   ---on Unix systems, represents the device type, for special file inodes. On Windows systems represents the same as dev
+  ---
   ---access
+  ---
   ---time of last access
+  ---
   ---modification
+  ---
   ---time of last data modification
+  ---
   ---change
+  ---
   ---time of last file status change
+  ---
   ---size
+  ---
   ---file size, in bytes
+  ---
   ---permissions
+  ---
   ---file permissions string
+  ---
   ---blocks
+  ---
   ---block allocated for file; (Unix only)
+  ---
   ---blksize
+  ---
   ---optimal file system I/O blocksize; (Unix only)
+  ---
   ---This function uses stat internally thus if the given filepath is a symbolic link, it is followed (if it points to another link the chain is followed recursively) and the information is about the file it refers to. To obtain information about the link itself, see function lfs.symlinkattributes.
   ---@param filepath string
   ---@param request_name_or_result_table string|table
@@ -71,6 +100,7 @@ local LFS = {
   attributes = function(filepath, request_name_or_result_table)
   end,
   ---Changes the current working directory to the given path.
+  ---
   ---Returns true in case of success or nil plus an error string.
   ---@param path string
   ---@return boolean|nil
@@ -78,6 +108,7 @@ local LFS = {
   chdir = function(path)
   end,
   ---Creates a lockfile (called lockfile.lfs) in path if it does not exist and returns the lock. If the lock already exists checks if it's stale, using the second parameter (default for the second parameter is INT_MAX, which in practice means the lock will never be stale. To free the the lock call lock:free().
+  ---
   ---In case of any errors it returns nil and the error message. In particular, if the lock exists and is not stale it returns the "File exists" message.
   ---@param path string
   ---@param seconds_stale? integer
@@ -97,6 +128,7 @@ local LFS = {
   dir = function(path)
   end,
   ---Locks a file or a part of it. This function works on open files; the file handle should be specified as the first argument. The string mode could be either r (for a read/shared lock) or w (for a write/exclusive lock). The optional arguments start and length can be used to specify a starting point and its length; both should be numbers.
+  ---
   ---Returns true if the operation was successful; in case of error, it returns nil plus an error string.
   ---@param filehandle file*
   ---@param mode "r"|"w"
@@ -113,6 +145,7 @@ local LFS = {
   link = function(old, new, symlink)
   end,
   ---Creates a new directory. The argument is the name of the new directory.
+  ---
   ---Returns true in case of success or nil, an error message and a system-dependent error code in case of error.
   ---@param dirname string
   ---@return boolean|nil
@@ -120,6 +153,7 @@ local LFS = {
   mkdir = function(dirname)
   end,
   ---Removes an existing directory. The argument is the name of the directory.
+  ---
   ---Returns true in case of success or nil, an error message and a system-dependent error code in case of error.
   ---@param dirname string
   ---@return boolean|nil
@@ -141,6 +175,7 @@ local LFS = {
   symlinkattributes = function(filepath, request_name)
   end,
   ---Set access and modification times of a file. This function is a bind to utime function. The first argument is the filename, the second argument (atime) is the access time, and the third argument (mtime) is the modification time. Both times are provided in seconds (which should be generated with Lua standard function os.time). If the modification time is omitted, the access time provided is used; if both times are omitted, the current time is used.
+  ---
   ---Returns true in case of success or nil, an error message and a system-dependent error code in case of error.
   ---@param filepath string
   ---@param atime? number
@@ -150,6 +185,7 @@ local LFS = {
   touch = function(filepath, atime, mtime)
   end,
   ---Unlocks a file or a part of it. This function works on open files; the file handle should be specified as the first argument. The optional arguments start and length can be used to specify a starting point and its length; both should be numbers.
+  ---
   ---Returns true if the operation was successful; in case of error, it returns nil plus an error string.
   ---@param filehandle file*
   ---@param start? integer
