33bc67b63e89a789baca310299173d9c18454903
JanSharp <steinmeister36@gmail.com>
2021-04-27 17:21:07 +0200

    Add custom.lua with all undefined classes

--- emmy_lua_generator.lua
+++ emmy_lua_generator.lua
@@ -440,6 +440,87 @@ local function generate_concepts()
 ]])
 end
 
+local function generate_custom()
+  write_file_to_target("custom.lua", file_prefix..[[
+---on_script_path_request_finished
+---@class Waypoint
+
+---script_raised_set_tiles
+---
+---LuaSurface.set_tile param tiles
+---@class Tile
+
+---LuaBootstrap.on_event param event
+---@class Event
+
+---LuaBootstrap.on_event param filters
+---
+---LuaBootstrap.set_event_filters param filters
+---@class Filters
+
+---LuaControl.crafting_queue
+---@class CraftingQueueItem
+
+---LuaControl.get_blueprint_entities
+---
+---LuaItemStack.get_blueprint_entities return
+---
+---LuaItemStack.set_blueprint_entities param entities
+---@class blueprint-entity
+
+---LuaItemStack.get_blueprint_tiles return
+---
+---LuaItemStack.set_blueprint_tiles param tiles
+---@class blueprint-tile
+
+---LuaEntity.circuit_connection_definitions
+---@class CircuitConnectionDefinition
+
+---LuaEntityPrototype.result_units
+---@class UnitSpawnDefinition
+
+---LuaGameScript.map_gen_presets
+---@class MapGenPreset
+
+---LuaGuiElement.elem_filters
+---
+---LuaGuiElement.add arg field elem_filters
+---@class PrototypeFilters
+
+---LuaGuiElement.tabs
+---@class TabAndContent
+
+---LuaHeatEnergySourcePrototype.connections
+---@class Connection
+
+---LuaItemStack.blueprint_icons
+---
+---LuaItemStack.default_icons
+---@class Icon
+
+---LuaLazyLoadedValue.get return
+---@class varies
+
+---LuaPlayer.get_alerts return
+---@class alert
+
+---LuaRemote.call param ...
+---@class variadic
+
+---LuaRemote.call return
+---@class Anything
+
+---LuaRendering.draw_polygon arg field vertices
+---@class CustomScriptRenderTarget
+
+---LuaSurface.create_decoratives arg field decoratives
+---@class Decorative
+
+---LuaSurface.find_decoratives_filtered return
+---@class DecorativeResult
+]])
+end
+
 ---@param _args Args
 ---@param _data ApiFormat
 local function generate(_args, _data)
@@ -451,6 +532,7 @@ local function generate(_args, _data)
   generate_events()
   generate_classes()
   generate_concepts()
+  generate_custom()
   delete_invalid_files_from_target()
   args = nil
   data = nil
