3f36b5c7e6a0cde2402e57c5dc11b5ce4c27ef59
JanSharp <steinmeister36@gmail.com>
2021-05-05 03:04:10 +0200

    Fix caching and improve non cached performance
    if --debug-api-json-crc was set the new crc32 would not get evaluated if
      the cache was outdated resulting in the wrong crc32 in the cache file
    The crc32 of the runtime-api.json was evaluated but never used if
      caching was disabled

--- main.lua
+++ main.lua
@@ -10,31 +10,36 @@ local args = args_service.get_args(arg)
 
 if not args.source_path:exists() then return end
 
-local api_json = file.read_all_text(args.source_path):gsub("<br/>", "\\n")
-local api_json_crc = args.debug_api_json_crc or crc32(api_json)
----@type ApiFormat
-local api_data
----@type Path
-local api_cache_path
----@type Path
-local api_cache_crc_path
+local api_json --- @type string
+local function get_api_json()
+  if not api_json then
+    api_json = file.read_all_text(args.source_path):gsub("<br/>", "\\n")
+  end
+  return api_json
+end
+local api_json_crc ---@type integer
+local api_data ---@type ApiFormat
+local api_cache_path ---@type Path
+local api_cache_crc_path ---@type Path
 
 if args.cache_dir_path then
+  api_json_crc = args.debug_api_json_crc or crc32(get_api_json())
   api_cache_path = args.cache_dir_path / "api_cache.lua"
   api_cache_crc_path = args.cache_dir_path / "api_cache_crc"
   if api_cache_path:exists() and api_cache_crc_path:exists() then
     local cache_crc = tonumber(file.read_all_text(api_cache_crc_path))
     if cache_crc == api_json_crc then
-      ---@type ApiFormat
-      api_data = loadfile(api_cache_path:str(), "t")()
+      api_data = loadfile(api_cache_path:str(), "t")() ---@type ApiFormat
     end
   end
 end
 
 if not api_data then
-  ---@type ApiFormat
-  api_data = json.decode(api_json)
+  api_data = json.decode(get_api_json()) ---@type ApiFormat
   if args.cache_dir_path then
+    if args.debug_api_json_crc then
+      api_json_crc = crc32(get_api_json())
+    end
     file.write_all_text(api_cache_path, serpent.dump(api_data))
     file.write_all_text(api_cache_crc_path, tostring(api_json_crc))
   end
